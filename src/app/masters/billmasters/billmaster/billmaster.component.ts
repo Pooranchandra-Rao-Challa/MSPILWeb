9935f1a589c230bc96ab1443fc37c405bb43fda1
SaiKarthik1006 <riya@cis10137>
2023-01-31 17:54:37 +0530

    design for bill master and bill params screen.

--- src/app/masters/billmasters/billmaster/billmaster.component.ts
+++ src/app/masters/billmasters/billmaster/billmaster.component.ts
@@ -1,5 +1,6 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import { Table } from 'primeng/table';
 @Component({
   selector: 'app-billmaster',
   templateUrl: './billmaster.component.html',
@@ -7,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BillMasterComponent implements OnInit {
+  billMasters: any;
+  loading: boolean = false;
+  filter: any;
+  showDialog: boolean = false;
+  fbBillMaster!: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.billmasterForm();
+  }
+
+  billmasterForm() {
+    this.fbBillMaster = this.formbuilder.group({
+      category: ['', (Validators.required)],
+      season: ['', (Validators.required)],
+      fromDate: ['', (Validators.required)],
+      toDate: ['', (Validators.required)],
+      isFinal: [true, Validators.requiredTrue]
+    });
+  }
+
+  get FormControls() {
+    return this.fbBillMaster.controls;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = '';
+  }
+
+  onSubmit() {
+    if (this.fbBillMaster.valid) {
+      console.log(this.fbBillMaster.value);
+    }
+    else {
+      this.fbBillMaster.markAllAsTouched();
+    }
   }
 
 }
