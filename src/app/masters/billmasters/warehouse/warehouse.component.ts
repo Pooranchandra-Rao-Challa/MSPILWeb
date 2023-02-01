96b240294c1ef596e1bd2a96e9a14d71788637c6
SaiKarthik1006 <riya@cis10137>
2023-02-01 12:06:39 +0530

    design for ware house screen.

--- src/app/masters/billmasters/warehouse/warehouse.component.ts
+++ src/app/masters/billmasters/warehouse/warehouse.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-warehouse',
@@ -7,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class WareHouseComponent implements OnInit {
+  wareHouse: any;
+  loading: boolean = false;
+  filter: any;
+  showDialog: boolean = false;
+  fbWareHouse!: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.dieselRateForm();
+  }
+
+  dieselRateForm() {
+    this.fbWareHouse = this.formbuilder.group({
+      code: ['', (Validators.required)],
+      name: ['', (Validators.required)],
+      glcode: ['', (Validators.required)],
+      subglcode: ['', Validators.required],
+      isActive: [true, Validators.requiredTrue]
+    });
+  }
+
+  get FormControls() {
+    return this.fbWareHouse.controls;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = '';
+  }
+
+  onSubmit() {
+    if (this.fbWareHouse.valid) {
+      console.log(this.fbWareHouse.value);
+    }
+    else {
+      this.fbWareHouse.markAllAsTouched();
+    }
   }
 
 }
