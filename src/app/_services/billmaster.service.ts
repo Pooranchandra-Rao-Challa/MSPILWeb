795315b943347952a74cf4fb9bf4b63516ac68ef
SaiKarthik1006 <riya@cis10137>
2023-02-06 10:44:45 +0530

    updated code for bills, bill params, diesel bunk.

--- src/app/_services/billmaster.service.ts
+++ src/app/_services/billmaster.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
-import { BillDto, BillParameterDto, BillParameterViewDto, BillViewDto, DieselBunkViewDto } from "../_models/billingmaster";
+import { BillDto, BillParameterDto, BillParameterViewDto, BillViewDto, DieselBunkDto, DieselBunkViewDto } from "../_models/billingmaster";
 import { ApiHttpService } from "./api.http.service";
-import { CREATE_BILLPARAM_URI, CREATE_BILL_URI, GET_BILLPARAMS_URI, GET_BILLS_URI, GET_DIESEL_BUNK_URI, UPDATE_BILLPARAM_URI, UPDATE_BILL_URI } from "./api.uri.service";
+import { CREATE_BILLPARAM_URI, CREATE_BILL_URI, CREATE_DIESELBUNK_URI, GET_BILLPARAMS_URI, GET_BILLS_URI, GET_DIESEL_BUNK_URI, UPDATE_BILLPARAM_URI, UPDATE_BILL_URI,
+  UPDATE_DIESELBUNK_URI } from "./api.uri.service";
 
 
 @Injectable({ providedIn: 'root' })
 export class BillMasterService extends ApiHttpService {
+
+  public GetBills() {
+    return this.get<BillViewDto[]>(GET_BILLS_URI);
+  }
 
   public CreateBill(bill: any) {
     return this.post<any>(CREATE_BILL_URI, bill);
@@ -15,12 +20,11 @@ export class BillMasterService extends ApiHttpService {
     return this.post<any>(UPDATE_BILL_URI, bill);
   }
 
-  public GetBills() {
-    return this.get<BillViewDto[]>(GET_BILLS_URI);
+  public GetBillParameters() {
+    return this.get<BillParameterViewDto[]>(GET_BILLPARAMS_URI);
   }
 
   public CreateBillParam(billParam: BillParameterDto) {
-    debugger
     return this.post<BillParameterDto>(CREATE_BILLPARAM_URI, billParam);
   }
 
@@ -32,8 +36,13 @@ export class BillMasterService extends ApiHttpService {
     return this.get<DieselBunkViewDto[]>(GET_DIESEL_BUNK_URI);
   }
 
-  public GetBillParameters() {
-    return this.get<BillParameterViewDto[]>(GET_BILLPARAMS_URI);
+  public CreateDieselBunk(dieselBunk: DieselBunkDto) {
+    debugger
+    return this.post<DieselBunkDto>(CREATE_DIESELBUNK_URI, dieselBunk);
+  }
+
+  public UpdateDieselBunk(dieselBunk: DieselBunkDto) {
+    return this.post<DieselBunkDto>(UPDATE_DIESELBUNK_URI, dieselBunk);
   }
 
 }
