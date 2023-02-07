b8cd9f9e901151387ff56e67e81f466749b07235
SaiKarthik1006 <riya@cis10137>
2023-02-07 09:59:20 +0530

    services and models for diesel rate and distance rate

--- src/app/_services/billmaster.service.ts
+++ src/app/_services/billmaster.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@angular/core";
-import { BillDto, BillParameterDto, BillParameterViewDto, BillViewDto, DieselBunkDto, DieselBunkViewDto } from "../_models/billingmaster";
+import { BillDto, BillParameterDto, BillParameterViewDto, BillViewDto, DieselBunkDto, DieselBunkViewDto, DieselRateDto, DieselRateViewDto, DistanceRateDto, DistanceRateViewDto } from "../_models/billingmaster";
 import { ApiHttpService } from "./api.http.service";
-import { CREATE_BILLPARAM_URI, CREATE_BILL_URI, CREATE_DIESELBUNK_URI, GET_BILLPARAMS_URI, GET_BILLS_URI, GET_DIESEL_BUNK_URI, UPDATE_BILLPARAM_URI, UPDATE_BILL_URI,
-  UPDATE_DIESELBUNK_URI } from "./api.uri.service";
+import {
+  CREATE_BILLPARAM_URI, CREATE_BILL_URI, CREATE_DIESELBUNK_URI, CREATE_DIESELRATE_URI, CREATE_DISTANCERATE_URI, GET_BILLPARAMS_URI, GET_BILLS_URI, GET_DIESELRATES_URI, GET_DIESEL_BUNK_URI, GET_DISTANCERATES_URI, UPDATE_BILLPARAM_URI, UPDATE_BILL_URI,
+  UPDATE_DIESELBUNK_URI,
+  UPDATE_DIESELRATE_URI,
+  UPDATE_DISTANCERATE_URI
+} from "./api.uri.service";
 
 
 @Injectable({ providedIn: 'root' })
@@ -43,6 +47,36 @@ export class BillMasterService extends ApiHttpService {
 
   public UpdateDieselBunk(dieselBunk: DieselBunkDto) {
     return this.post<DieselBunkDto>(UPDATE_DIESELBUNK_URI, dieselBunk);
+  }
+
+  // Diesel rates services
+
+  public GetDieselRates() {
+    return this.get<DieselRateViewDto[]>(GET_DIESELRATES_URI);
+  }
+
+  public CreateDieselRate(dieselRate: DieselRateDto) {
+    debugger
+    return this.post<DieselRateDto>(CREATE_DIESELRATE_URI, dieselRate);
+  }
+
+  public UpdateDieselRate(dieselRate: DieselRateDto) {
+    return this.post<DieselRateDto>(UPDATE_DIESELRATE_URI, dieselRate);
+  }
+
+  // Distance rate services
+
+  public GetDistanceRates() {
+    return this.get<DistanceRateViewDto[]>(GET_DISTANCERATES_URI);
+  }
+
+  public CreateDistanceRate(distanceRate: DistanceRateDto) {
+    debugger
+    return this.post<DistanceRateDto>(CREATE_DISTANCERATE_URI, distanceRate);
+  }
+
+  public UpdateDistanceRate(distanceRate: DistanceRateDto) {
+    return this.post<DistanceRateDto>(UPDATE_DISTANCERATE_URI, distanceRate);
   }
 
 }
