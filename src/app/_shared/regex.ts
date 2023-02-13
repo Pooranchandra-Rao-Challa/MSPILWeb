32ebddca9570e52d3edf1fecbba1c34e25797fab
SaiKarthik1006 <riya@cis10137>
2023-02-13 10:10:48 +0530

    regular expersions.

---
+++ src/app/_shared/regex.ts
@@ -0,0 +1,5 @@
+/* Regular Expersions */
+export const alphaOnlyReg = '[a-zA-Z ]*';
+export const numericOnlyReg = '[0-9]*';
+export const alphaNumericReg = '[a-zA-Z0-9 ]*';
+export const phoneNoReg = '[(][7-9][0-9]{2}[)][-][0-9]{3}[-][0-9]{4}';
