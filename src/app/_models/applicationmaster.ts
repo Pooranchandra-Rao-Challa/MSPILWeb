032a509dfba40b6184e175784799a670f982774c
SaiKarthik1006 <riya@cis10137>
2023-02-14 10:14:21 +0530

    updated serives and models for variety and vehicle.

--- src/app/_models/applicationmaster.ts
+++ src/app/_models/applicationmaster.ts
@@ -1,20 +1,20 @@
-export class LookUpHeaderDto{
-  id?:number
+export class LookUpHeaderDto {
+  id?: number
   code?: string;
   name?: string;
   isActive?: boolean;
-  lookUpDetails?:LookupDetailDto[];
+  lookUpDetails?: LookupDetailDto[];
   LookupDetailDto: any;
 }
-export class LookupDetailDto{
-  id?:number;
+export class LookupDetailDto {
+  id?: number;
   code?: string;
   name?: string;
   remarks?: string;
   listingorder?: number;
 }
 
-export class LookupViewDto{
+export class LookupViewDto {
   id?: number;
   code?: string;
   name?: string;
@@ -25,3 +25,57 @@ export class LookupViewDto{
   createdBy?: string;
 }
 
+export class VarietyDto {
+  varietyId?: number;
+  code?: string;
+  name?: string;
+  varietyTypeId?: number;
+  plantAge?: number;
+  ratoonAge?: number;
+  sugarContent?: string;
+  plantSuitability?: string;
+  isActive: boolean = true;
+}
+
+export class VarietyViewDto {
+  varietyId?: number;
+  code?: string;
+  name?: string;
+  varietyTypeId?: number;
+  varietyName?: string;
+  plantAge?: number;
+  ratoonAge?: number;
+  sugarContent?: string;
+  plantSuitability?: string;
+  isActive: boolean = true;
+  updatedAt?: Date;
+  createdAt?: Date;
+  updatedBy?: string;
+  createdBy?: string;
+}
+
+export class VehicleTypeDto {
+  vehicleTypeId?: number;
+  code?: string;
+  name?: string;
+  capacity?: number;
+  billingCapacity?: number;
+  bindingCane?: number;
+  badCane?: number;
+  isActive?: boolean;
+}
+
+export class VehicleTypeViewDto {
+  vehicleTypeId?: number;
+  code?: string;
+  name?: string;
+  capacity?: number;
+  billingCapacity?: number;
+  bindingCane?: number;
+  badCane?: number;
+  isActive?: boolean;
+  updatedAt?: Date;
+  createdAt?: Date;
+  updatedBy?: string;
+  createdBy?: string;
+}
--- src/app/_services/appmaster.service.ts
+++ src/app/_services/appmaster.service.ts
@@ -1,22 +1,48 @@
+import { VarietyViewDto, VarietyDto, VehicleTypeViewDto, VehicleTypeDto } from './../_models/applicationmaster';
 import { Injectable } from "@angular/core";
 import { LookUpHeaderDto, LookupViewDto } from "../_models/applicationmaster";
 import { ApiHttpService } from "./api.http.service";
-import { CREATE_LOOKUP_URI, GET_LOOKUP_URI, GET_PLANTTYPE_URI, UPDATE_LOOKUP_URI } from "./api.uri.service";
+import { CREATE_LOOKUP_URI, CREATE_VARIETY_URI, CREATE_VEHICLE_TYPE_URI, GET_LOOKUP_URI, GET_PLANTTYPE_URI, GET_VARIETY_URI, GET_VEHICLE_TYPE_URI, UPDATE_LOOKUP_URI, UPDATE_VARIETY_URI, UPDATE_VEHICLE_TYPE_URI } from "./api.uri.service";
 
 
 
 @Injectable({ providedIn: 'root' })
 export class AppMasterService extends ApiHttpService {
-// lookup
-public GetlookUp() {
+  // lookup
+  public GetlookUp() {
     return this.get<LookupViewDto[]>(GET_LOOKUP_URI);
-    }
-    public Createlookup(lookup:LookUpHeaderDto){
-      return this.post<LookUpHeaderDto>(CREATE_LOOKUP_URI,lookup);
-    }
-    public Updatelookup(lookup: LookUpHeaderDto){
-      return this.post<LookUpHeaderDto>(UPDATE_LOOKUP_URI,lookup);
-    }
+  }
+  public Createlookup(lookup: LookUpHeaderDto) {
+    return this.post<LookUpHeaderDto>(CREATE_LOOKUP_URI, lookup);
+  }
+  public Updatelookup(lookup: LookUpHeaderDto) {
+    return this.post<LookUpHeaderDto>(UPDATE_LOOKUP_URI, lookup);
+  }
 
-    
+  /* Variety */
+
+  public GetVarieties() {
+    return this.get<VarietyViewDto[]>(GET_VARIETY_URI);
+  }
+  public CreateVariety(variety: VarietyDto) {
+    return this.post<VarietyDto>(CREATE_VARIETY_URI, variety);
+  }
+  public UpdateVariety(variety: VarietyDto) {
+    return this.post<VarietyDto>(UPDATE_VARIETY_URI, variety);
+  }
+
+  /* Vehicle Type */
+
+  public GetVehicleTypes() {
+    return this.get<VehicleTypeViewDto[]>(GET_VEHICLE_TYPE_URI);
+  }
+  public CreateVehicleType(vehicleType: VehicleTypeDto) {
+    return this.post<VehicleTypeDto>(CREATE_VEHICLE_TYPE_URI, vehicleType);
+  }
+  public UpdateVehicleType(vehicleType: VehicleTypeDto) {
+    return this.post<VehicleTypeDto>(UPDATE_VEHICLE_TYPE_URI, vehicleType);
+  }
+
 }
+
+
