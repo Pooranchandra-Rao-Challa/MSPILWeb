91be064c372c19f32444c54a5f21b9add612bc05
sadikh <sadikh.sharieff@calibrage.in>
2023-02-17 10:41:51 +0530

    sampleslab page added

---
+++ src/app/account/requestforgotpassword/requestforgotpassword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requestforgotpassword',
+  templateUrl: './requestforgotpassword.component.html',
+  styleUrls: ['./requestforgotpassword.component.scss']
+})
+export class RequestforgotpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
