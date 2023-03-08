89e7b706be023e263765d20a521aa39549f362f6
SaiKarthik1006 <riya@cis10137>
2023-03-08 17:56:58 +0530

    updated uri endpoint with IDANDPARAM1 and WITH_PARAM.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,3 +6,5 @@ export const environment = {
 };
 export const URI_ENDPOINT = (term:string) => `${environment.ApiUrl}${term}`;
 export const URI_ENDPOINT_WITH_ID = (term:string,id: any) => `${environment.ApiUrl}${term}/${id}`;
+export const URI_ENDPOINT_WITH_IDANDPARAM1 = (term:string,id: any,param1:string) => `${environment.ApiUrl}${term}/${id}/${param1}`;
+export const URI_ENDPOINT_WITH_PARAM = (term:string,param: any) => `${environment.ApiUrl}${term}/${param}`;
