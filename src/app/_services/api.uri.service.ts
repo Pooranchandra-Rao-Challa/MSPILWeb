d75a6d7ffa36cde208cb005562cfb4fdda8abff2
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2023-04-18 20:32:41 +0530

    Forgot password code updated

--- src/app/_services/api.uri.service.ts
+++ src/app/_services/api.uri.service.ts
@@ -16,6 +16,7 @@ export const GET_ROLES_URI = "Security/GetRoles"
 export const CREATE_ROLE_URI = "Security/CreateRole"
 export const UPDATE_ROLE_URI = "Security/UpdateRole"
 export const USER_SECURITY_QUESTIONS = "Security/UserQuestions";
+export const FORGOT_PASSWORD = "Security/Forgotpassword";
 export const GET_ROLE_PERMISSIONS_URI = "Security/GetRolePermissions"
 export const GET_PERMISSIONS_URI = "Security/GetPermissions"
 export const CREATE_DISTRICT_URI = "GeoMaster/CreateDistrict"
