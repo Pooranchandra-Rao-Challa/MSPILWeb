3212751458f251eebbf68f8f63db02ceb3409320
SaiKarthik1006 <riya@cis10137>
2023-04-27 19:18:05 +0530

    directive for address field.

---
+++ src/app/_directives/address.directive.ts
@@ -0,0 +1,39 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[address]'
+})
+export class AddressDirective {
+
+  private regex: RegExp = new RegExp(/^[-a-zA-Z0-9-/.;:,_() ]+(\s+[-a-zA-Z0-9-/.;:,_() ]+)*$/);
+
+  private specialKeys: Array<string> = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Tab', 'End', 'Home'];
+  constructor(private el: ElementRef) {
+
+  }
+  /* Key board action */
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    // Allow Backspace, tab, end, and home keys
+    if (this.specialKeys.indexOf(event.key) !== -1) {
+      return;
+    }
+    let current: string = this.el.nativeElement.value;
+    let next: string = current.concat(event.key);
+    if (next && !String(next).match(this.regex)) {
+      event.preventDefault();
+    }
+  }
+
+  /* Copy paste action */
+  @HostListener('paste', ['$event'])
+  onPaste(event: any) {
+    const clipboardData = (event.originalEvent || event).clipboardData.getData("text/plain");
+    if (clipboardData) {
+      if (!this.regex.test(clipboardData)) {
+        event.preventDefault();
+      }
+    }
+    return
+  }
+}
