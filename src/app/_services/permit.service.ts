1bb47859fefd4899b31ca76d61729d2c14d557d7
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-04-28 14:43:26 +0530

    changes related to cutting order

--- src/app/_services/permit.service.ts
+++ src/app/_services/permit.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { CircleforUserDto, DivisionsforUserDto, EstimatedViewDto, ExcessTonDto, ExcessTonViewDto, ExcessViewDto, FarmersDto, FarmersInPlotsForUserDto, IPlotScheduleViewDto, ISeasonScheduleGroupViewDto, PlantTypeForUserDto, PlotCuttingOrderViewDto, PlotQuotaViewDto, PlotsForUserDto, ScheduleGroupDto, ScheduleGroupPlotsDto, SeasonCuttingOrderViewDto, SeasonQuotaViewDto, SectionforUserDto, VarietiesForUserDto, VillageforUserDto, } from "../_models/permits";
+import { CircleforUserDto, DivisionsforUserDto, EstimatedViewDto, ExcessTonDto, ExcessTonViewDto, ExcessViewDto, FarmersDto, FarmersInPlotsForUserDto, GetCuttingOrderViewDto, IPlotScheduleViewDto, ISeasonScheduleGroupViewDto, PlantTypeForUserDto, PlotCuttingOrderViewDto, PlotQuotaViewDto, PlotsForUserDto, ScheduleGroupDto, ScheduleGroupPlotsDto, SeasonCuttingOrderViewDto, SeasonQuotaViewDto, SectionforUserDto, VarietiesForUserDto, VillageforUserDto, } from "../_models/permits";
 import { ApiHttpService } from "./api.http.service";
-import { CREATE_EXCESS_TON, CREATE_SCHEDULE_GROUP_URI, GET_CIRCLES_FOR_USER, GET_DIVISIONS_FOR_USER, GET_ESTIMATED_TON, GET_EXCESS_TON, GET_FARMERS_FOR_ESTIMATED_TON, GET_FARMERS_IN_PLOTS_FOR_USER, GET_FARMER_PLOTS_IN_SCHEDULE_URI, GET_PLANT_TYPE_FOR_USER, GET_PLOTS_FOR_USER, GET_PLOT_CUTTING_ORDERS, GET_PLOT_QUOTAS_URI, GET_SCHEDULE_GROUP_PLOTS_URI, GET_SEASON_CUTTING_ORDERS, GET_SEASON_QUOTAS, GET_SEASON_SCHEDULE_GROUPS_URI,  GET_SECTIONS_FOR_USER,  GET_VARIETIES_FOR_USER,  GET_VILLAGES_FOR_USER,UPDATE_EXCESS_TONNAGE, } from "./api.uri.service";
+import { CREATE_EXCESS_TON, CREATE_SCHEDULE_GROUP_URI, GET_CIRCLES_FOR_USER, GET_CUTTING_ORDERS, GET_DIVISIONS_FOR_USER, GET_ESTIMATED_TON, GET_EXCESS_TON, GET_FARMERS_FOR_ESTIMATED_TON, GET_FARMERS_IN_PLOTS_FOR_USER, GET_FARMER_PLOTS_IN_SCHEDULE_URI, GET_PLANT_TYPE_FOR_USER, GET_PLOTS_FOR_USER, GET_PLOT_CUTTING_ORDERS, GET_PLOT_QUOTAS_URI, GET_SCHEDULE_GROUP_PLOTS_URI, GET_SEASON_CUTTING_ORDERS, GET_SEASON_QUOTAS, GET_SEASON_SCHEDULE_GROUPS_URI,  GET_SECTIONS_FOR_USER,  GET_VARIETIES_FOR_USER,  GET_VILLAGES_FOR_USER,UPDATE_EXCESS_TONNAGE, } from "./api.uri.service";
 
 
 
@@ -102,6 +102,9 @@ public GetSeasonCuttingOrder(seasonId: number) {
 }
 public GetPlotCuttingOrder(seasonId:any,seasonCuttingOrderId: any) {
   return this.getWithParams<PlotCuttingOrderViewDto>(GET_PLOT_CUTTING_ORDERS,[seasonId,seasonCuttingOrderId]);
+}
+public GetCuttingOrder(cuttingorder: GetCuttingOrderViewDto) {
+  return this.post<GetCuttingOrderViewDto>(GET_CUTTING_ORDERS, cuttingorder);
 }
 
 public GetSeasonQuotas(seasonId: number) {
