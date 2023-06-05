c6136a018e835974dc2f5b7ac4b0ab1623510032
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-06-05 18:09:41 +0530

    chenges related to users screen

--- src/app/security/users/users.component.ts
+++ src/app/security/users/users.component.ts
@@ -10,6 +10,7 @@ import { HttpEvent } from '@angular/common/http';
 import { RG_ALPHA_ONLY, RG_EMAIL, RG_PHONE_NO, RG_NUMERIC_ONLY, MIN_LENGTH_2, MAX_LENGTH_50, MAX_LENGTH_15, IP_ADDRESS_NO } from 'src/app/_shared/regex';
 import { AlertMessage, ALERT_CODES } from 'src/app/_alerts/alertMessage';
 import { JWTService } from 'src/app/_services/jwt.service';
+import { ITableHeader } from 'src/app/_models/common';
 
 @Component({
   selector: 'app-users',
@@ -18,39 +19,58 @@ import { JWTService } from 'src/app/_services/jwt.service';
 })
 export class UsersComponent implements OnInit {
   @ViewChild('filter') filter!: ElementRef;
-  users: UserViewDto[] =[];
+  users: UserViewDto[] = [];
   selectedUser: UserViewDto = {};
   user: UserDto = {};
   roles: RoleDto[] = [];
   dialog: boolean = false;
   submitLabel!: string;
-  globalFilters: string[] = ["userId", "userName", "firstName", "lastName", "email", "mobileNumber", "roleId", "imeino", "ipaddress", "iprestriction",
-  "isAdminGate", "isGross", "isTare", "isDumpYard", "isActive",  "createdAt",  "updatedAt",];
+  globalFilters: string[] = ['userId', 'userName', 'firstName', 'lastName', 'email', 'mobileNumber', 'roleName', 'imeino', 'ipaddress', 'iprestriction',
+    'isAdminGate', 'isGross', 'isTare', 'isDumpYard', 'isActive', 'createdAt', 'updatedAt'];
   userForm!: FormGroup;
   mediumDate: string = MEDIUM_DATE;
   addFlag: boolean = true;
-  permissions:any
-  
+  permissions: any
+
   constructor(private formbuilder: FormBuilder,
     private securityService: SecurityService,
     private alertMessage: AlertMessage,
-    private jwtService:JWTService) {
+    private jwtService: JWTService) {
   }
-  
+
+  headers: ITableHeader[] = [
+    { field: 'userId', header: 'userId', label: 'User Id' },
+    { field: 'userName', header: 'userName', label: 'User Name' },
+    { field: 'firstName', header: 'firstName', label: 'First Name' },
+    { field: 'lastName', header: 'lastName', label: 'Last Name' },
+    { field: 'email', header: 'email', label: 'Email Id' },
+    { field: 'mobileNumber', header: 'mobileNumber', label: 'Mobile Number' },
+    { field: 'roleName', header: 'roleName', label: 'Role' },
+    { field: 'imeino', header: 'imeino', label: 'IMEI NO' },
+    { field: 'ipaddress', header: 'ipaddress', label: 'IP Address' },
+    { field: 'iprestriction', header: 'iprestriction', label: 'IP Restriction' },
+    { field: 'isAdminGate', header: 'isAdminGate', label: 'Is AdminGate' },
+    { field: 'isGross', header: 'isGross', label: 'Is Gross' },
+    { field: 'isTare', header: 'isTare', label: 'Is Tare' },
+    { field: 'isDumpYard', header: 'isDumpYard', label: 'Is DumpYard' },
+    { field: 'isActive', header: 'isActive', label: 'Is Active' },
+    { field: 'createdAt', header: 'createdAt', label: 'Created Date' },
+    { field: 'updatedAt', header: 'updatedAt', label: 'Updated Date' },
+  ];
   ngOnInit(): void {
     this.permissions = this.jwtService.Permissions;
     this.userForm = this.formbuilder.group({
       userId: ['',],
       isAdmin: [false],
-      userName:  new FormControl('', [Validators.required, Validators.minLength(MIN_LENGTH_2),Validators.maxLength(MAX_LENGTH_50)]),
+      userName: new FormControl('', [Validators.required, Validators.minLength(MIN_LENGTH_2), Validators.maxLength(MAX_LENGTH_50)]),
       password: ['', (Validators.required)],
-      firstName: new FormControl('', [Validators.required, Validators.pattern(RG_ALPHA_ONLY),Validators.minLength(MIN_LENGTH_2),Validators.maxLength(MAX_LENGTH_50)]),
-      lastName: new FormControl('', [Validators.required, Validators.pattern(RG_ALPHA_ONLY),Validators.minLength(MIN_LENGTH_2),Validators.maxLength(MAX_LENGTH_50)]),
-      email: new FormControl('', [Validators.required,Validators.pattern(RG_EMAIL)]),
+      firstName: new FormControl('', [Validators.required, Validators.pattern(RG_ALPHA_ONLY), Validators.minLength(MIN_LENGTH_2), Validators.maxLength(MAX_LENGTH_50)]),
+      lastName: new FormControl('', [Validators.required, Validators.pattern(RG_ALPHA_ONLY), Validators.minLength(MIN_LENGTH_2), Validators.maxLength(MAX_LENGTH_50)]),
+      email: new FormControl('', [Validators.required, Validators.pattern(RG_EMAIL)]),
       mobileNumber: new FormControl('', [Validators.required, Validators.pattern(RG_PHONE_NO)]),
       roleId: ['', (Validators.required)],
-      ipaddress: [''],
-      imeino: new FormControl('', [Validators.pattern(RG_NUMERIC_ONLY),Validators.minLength(MAX_LENGTH_15)]),
+      ipaddress: ['', (Validators.pattern(IP_ADDRESS_NO))],
+      imeino: new FormControl('', [Validators.pattern(RG_NUMERIC_ONLY), Validators.minLength(MAX_LENGTH_15)]),
       iprestriction: [false],
       isAdminGate: [false],
       isGross: [false],
@@ -67,7 +87,7 @@ export class UsersComponent implements OnInit {
   initUsers() {
     this.securityService.GetUsers().subscribe(resp => {
       this.users = resp as unknown as UserViewDto[];
-      this.users.sort((a,b)=> (a.userName||"").localeCompare(b.userName||""))
+      this.users.sort((a, b) => (a.userName || "").localeCompare(b.userName || ""))
       console.log(this.users);
 
     })
@@ -84,13 +104,14 @@ export class UsersComponent implements OnInit {
     if (this.selectedUser && this.selectedUser.userId) {
       this.submitLabel = "Update User";
       this.securityService.GetUserWithSections(this.selectedUser.userId).subscribe(resp => {
-        this.user = resp as unknown as UserDto;   
+        console.log(resp);
+        this.user = resp as unknown as UserDto;
         if (this.user.roles?.length == 1)
           this.user.roleId = this.user.roles[0].roleId;
         this.user.password = "";
         this.userForm.setValue(this.user);
-      //  this.userForm.get("password")?.disable();
-      })     
+        this.userForm.get("password")?.disable();
+      })
     } else {
       this.submitLabel = "Add User";
       this.user = {};
@@ -103,7 +124,7 @@ export class UsersComponent implements OnInit {
       this.user.isAdmin = false;
       this.userForm.get("password")?.enable();
       this.securityService.GetAllSections().subscribe(resp => {
-       this.user.userSections = resp as unknown as UserSectionDto[];
+        this.user.userSections = resp as unknown as UserSectionDto[];
       })
     }
   }
