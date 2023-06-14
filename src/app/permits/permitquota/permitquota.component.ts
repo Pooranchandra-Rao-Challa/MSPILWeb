0a6f917d3c407e98fcd52a85b029ce41d42d7af4
manikanta92-eng <123150632+manikanta92-eng@users.noreply.github.com>
2023-06-14 15:04:47 +0530

    Changes in permit Quata

--- src/app/permits/permitquota/permitquota.component.ts
+++ src/app/permits/permitquota/permitquota.component.ts
@@ -48,6 +48,7 @@ export class PermitQuotaComponent implements OnInit {
   toScheduleNo: any;
   editPermitQuotaData: any
   error: boolean = false;
+  isEditing: boolean = false;
 
   constructor(
     private formbuilder: FormBuilder,
@@ -129,6 +130,7 @@ export class PermitQuotaComponent implements OnInit {
     return this.fbPermitQuota.controls
   }
   getPermitQuota() {
+    this.isEditing = false;
     this.resetFormAndGrid()
     this.submitLabel = "Add Permit Quota";
     this.addFlag = true;
@@ -194,6 +196,7 @@ export class PermitQuotaComponent implements OnInit {
     });
   }
   editPermitQuota(permitQuota: SeasonQuotaViewDto) {
+    this.isEditing = true;
     this.addFlag = false;
     this.submitLabel = 'Update Permit Quota';
     this.editPermitQuotaData = permitQuota;
